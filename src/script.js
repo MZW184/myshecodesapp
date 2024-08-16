6660ab19e6dedf328dba08dd713de4dc21d989f8
MZW184 <134600467+MZW184@users.noreply.github.com>
2024-08-16 15:12:51 +0200

    search functionality using API

--- src/script.js
+++ src/script.js
@@ -1,9 +1,24 @@
+function refreshWeather(response) {
+  let temperature = document.querySelector("#current-temperature");
+  temperature.innerHTML = Math.round(response.data.daily[0].temperature.day);
+  let city = document.querySelector("#current-city");
+  city.innerHTML = response.data.city;
+}
+
+function searchCity(city) {
+  let apiKey = "248a5b083cf5484310345e4f53aedobt";
+  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?query=${city}&key=${apiKey}`;
+  axios.get(apiUrl).then(refreshWeather);
+}
+
 function searchResults(event) {
   event.preventDefault();
-  let city = document.querySelector("#current-city");
+
   let searchInput = document.querySelector("#search-input");
-  city.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchResults);
+
+searchCity("Paris");
